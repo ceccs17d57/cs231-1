fc026d6dd5461e25c3f16c3f1fc62ad137ab52fb
NikhilMTomy <nikhilmtomy@gmail.com>
2018-09-05 07:05:35 +0530

    Added selection sort

---
+++ 02_selection_sort.c
@@ -0,0 +1,40 @@
+/*
+ * 01
+ * Sequential Search
+ * Author : Nikhil M Tomy
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+	int i, j, pos, array[100], temp, size;
+
+	printf("Enter number of elements in the array : ");
+	scanf("%d", &size);
+
+	printf("Enter the array\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+	
+	for (i=0; i<size; i++) {
+		pos = i;
+
+		for (j=i; j<size; j++) {
+			if (array[j] < array[pos]) {
+				pos =j;
+			}
+		}
+		if (i != pos) {
+			temp = array[i];
+			array[i] = array[pos];
+			array[pos] = temp;
+		}
+	}
+
+	printf("Sorted array \n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+}
