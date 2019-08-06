ea539847de5b1005ac6ee24e96e4e9f954b30ec6
ceccs17d57 <43899059+ceccs17d57@users.noreply.github.com>
2019-08-06 21:20:37 +0530

    Add files via upload

---
+++ scheduling.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+void main()
+{
+int y=1,choice,bt[20],wt[20],n,i;
+printf("enter the no of process");
+scanf("%d",&n);
+printf("enter the burst time");
+for(i=0;i<n;i++)
+{
+	printf("p[%d]:",i+1);
+	scanf("%d",&bt[i]);
+}
+while(y=1)
+	{
+		printf("1.fcfs");
+		printf("2.sjf");
+		printf("3.round robin");
+		printf("4.priority");
+
+		printf("enter the choice");
+		scanf("%d",&choice);
+			switch (choice){
+					case 1: fcfs();
+						break;
+					case 2: sjf();
+						break;
+					case 3: roundrobin();
+						break;
+					case 4: priority();
+						break;
+					default: printf("enter the right choice");
+						 break;
+					}
+	}
+}
+void fcfs()
+{
+	waitingtime(wt,bt,n);
+	turnaroundtime(wt,bt,n);
+}
+void sjf()
+{
+sort(bt,n);
+waitingtime(wt,bt,n);
+turnaroundtime(wt,bt,n);
+}
+
+
+void waitingtime(int wt[],int bt[],int n)
+	{
+	int i,avgwt=0,j;
+	wt[0]=0;
+	for(i=1;i<n;i++)
+	{
+		wt[i]=0;
+		for(j=0;j<i;j++)
+		{
+			wt[i]=wt[i]+bt[j];
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		avgwt=avgwt+wt[i];
+	}
+	printf("average waiting time is %d ",(avgwt/n));
+	}
+
+void turnaroundtime(int wt[],int bt[],int n)
+	{
+		int i,tt[20],avgtt;
+		for(i=0;i<n;i++)
+		{
+		tt[i]=wt[i]+bt[i];
+		avgtt=avgtt+tt[i];
+		}
+		printf("average tt %d"(avgtt/n));
+	}
+void sort(int bt[],int n)
+	{
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=1;j<n;j++)
+			{
+				if(bt[i]>bt[j])
+				{
+					temp=bt[i];
+					bt[i]=bt[j];
+					bt[j]=temp;
+				}
+			}
+		}
+	}
+
+
